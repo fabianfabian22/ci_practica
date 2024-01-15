9df1c8c7d6b31cd7bec3adc89f7b5fdf4effe8ae
fabianfabian22 <fabianfabian22@hotmail.com>
2024-01-15 10:46:19 +0100

    inicipo del proyecto

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  _APIkey: "rQWEs987elkjhgiqpwi16548123",
+  _APIUrl: "http://miservidordev:8180/api",
+  _recaptcha: "qPO7643ASDjhaksdas_IIs",
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  _APIkey: "qASDw123eoqwpeiqpwi109233123",
+  _APIUrl: "http://localhost:81/api",
+  _recaptcha: "zOI2342ASDjhaksdas_IUy",
+};
